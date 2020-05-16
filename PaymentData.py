09b94aa33a026f5d6b4d62a1e79cef773b6e34bd
Robert Barrero <barrero.zizer.95@gmail.com>
2020-05-16 13:58:05 +0200

    probando con clase paymentData

--- PaymentData.py
+++ PaymentData.py
@@ -1,4 +1,17 @@
 class PaymentData:
 
-    def __init__(self):
+    def __init__(self, tipus, titular):
+
+        if tipus == "VISA" or "Mastercard":
+            self.tipusTargeta = tipus
+        else:
+            print("Tipus de targeta no vàlid.")
+
+        self.nomTitular = titular
+
+        def _init_Datos(numero, codi, total):
+            self.numeroTarjeta = numero
+            self.codiSeguretat = codi
+            self.importTotal = total
         pass
+
